ef9bf6929d9fd349841c6889e2b157e14a7a916f
youzouzou <guifang.you@tcl.com>
2021-09-01 16:20:28 +0800

    Update index.js

--- test-sprites-loader/index.js
+++ test-sprites-loader/index.js
@@ -15,8 +15,6 @@ module.exports = function (content, map, meta) {
     }
   }
 
-  console.log("图片数组", imageList.map(item=>item.path).join(""))
-
   Spritesmith.run({
     src: imageList.map(item=>item.path),
   },
@@ -29,24 +27,18 @@ module.exports = function (content, map, meta) {
         if (!fs.existsSync(path.resolve("./src/assets/sprites"))) {
           fs.mkdirSync(path.resolve("./src/assets/sprites")); // 创建放置雪碧图的目录
         }
-        console.log("结果", result)
-        console.log("路径", path.resolve("./src/assets/sprites/sprites.png"))
         fs.writeFileSync(path.resolve("./src/assets/sprites/sprites.png"), result.image); // 输出雪碧图
 
-        console.log("成功")
         // 对css中的背景图进行雪碧图定位替换
         imageList.forEach(item=>{
             // 匹配 ./assets/sprites/xxx.png
             const position = result.coordinates[path.resolve("src", item.id).replace(/\\/g,"/")]
             const originStr = "url\("+item.id.replace(/\\/g,"\\/")+"\)"
-            console.log(999, originStr)
             let reg = originStr.replace(/\//g,"\\/")
             reg = reg.replace(/\./g,"\\.")
             reg = reg.replace(/\(/g,"\\(")
             reg = reg.replace(/\)/g,"\\)")
-            console.log(888,reg, new RegExp(reg))
             content = content.replace(new RegExp(reg), "url("+path.resolve("./src/assets/sprites/sprites.png")+") "+(-position.x)+"px "+(-position.y)+"px");
-            console.log(666, content)
           })
 
           callback(null, content, map, meta);
