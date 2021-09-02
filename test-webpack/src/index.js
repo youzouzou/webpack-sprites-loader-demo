44b9c7f828b8e5865a6ecf188e296015bf9753b3
youzouzou <guifang.you@tcl.com>
2021-09-02 16:05:51 +0800

    update

--- test-webpack/src/index.js
+++ test-webpack/src/index.js
@@ -13,11 +13,9 @@ import './test2';
 // test();
 
 const d1 = document.createElement("div");
-d1.innerHTML = "hello~~~~~~~~1";
 d1.className = "sprite-1";
 document.body.appendChild(d1);
 
 const d2 = document.createElement("div");
-d2.innerHTML = "hello~~~~~~~~2";
 d2.className = "sprite-2";
 document.body.appendChild(d2);
--- test-webpack/webpack.config.js
+++ test-webpack/webpack.config.js
@@ -21,19 +21,24 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+        use: ['style-loader', 'css-loader', {
+          loader: path.resolve('../test-sprites-loader/index.js'),
+          options: {
+            dist: "./src/sprites" // 雪碧图保存路径（TODO:注意当output的clean设为true时，需要keep一下dist下面的路径，否则生成后可能会被删掉，应该是loader的执行时机问题）
+          }
+        },]
       },
-      {
-        test: /\.css$/i,
-        use: [
-          {
-            loader: path.resolve('../test-sprites-loader/index.js'),
-            options: {
-              dist: "./src/sprites" // 雪碧图保存路径（TODO:注意当output的clean设为true时，需要keep一下dist下面的路径，否则生成后可能会被删掉，应该是loader的执行时机问题）
-            }
-          },
-        ],
-      },
+      // {
+      //   test: /\.css$/i,
+      //   use: [
+      //     {
+      //       loader: path.resolve('../test-sprites-loader/index.js'),
+      //       options: {
+      //         dist: "./src/sprites" // 雪碧图保存路径（TODO:注意当output的clean设为true时，需要keep一下dist下面的路径，否则生成后可能会被删掉，应该是loader的执行时机问题）
+      //       }
+      //     },
+      //   ],
+      // },
     ]
   },
   optimization: {
