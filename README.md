798b2127a3f5a6f2bdf75cbba2e4c35d8f15a542
youzouzou <573257706@qq.com>
2021-09-02 22:47:34 +0800

    :smirk: update

--- README.md
+++ README.md
@@ -1,13 +1,25 @@
 # webpack-sprites-loader-demo
 
-cd test-sprites-loader
+### 方法一：
 
-yarn
+运行线上loader：https://www.npmjs.com/package/yzz-sprite-loader
 
-cd test-webpack
+`cd test-webpack`
 
-cd ..
+`yarn`
 
-yarn
+`yarn run dev`
 
-yarn run build
+### 方法二：
+
+运行本地loader
+
+`cd test-sprites-loader`
+
+`yarn`
+
+`cd ..`
+
+`cd test-webpack`
+
+`yarn run dev`
--- test-webpack/webpack.config.js
+++ test-webpack/webpack.config.js
@@ -1,34 +1,41 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: {
     index: "./src/index.js",
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
   output: {
-    filename: '[name].' + Date.now() + ".bundle.min.js",
-    path: path.resolve(__dirname, 'dist'),
-    clean: true
+    filename: "[name]." + Date.now() + ".bundle.min.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Webpack~"
+      title: "Webpack~",
     }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader', // 'yzz-sprite-loader'
-        {
-          loader: path.resolve('../test-sprites-loader/index.js'),
-          options: {
-            dist: "./src/sprites" // 雪碧图保存路径（TODO:注意当output的clean设为true时，需要keep一下dist下面的路径，否则生成后可能会被删掉，应该是loader的执行时机问题）
-          }
-        },
-      ]
+        use: [
+          "style-loader",
+          "css-loader",
+          "yzz-sprite-loader", // 线上loader：https://www.npmjs.com/package/yzz-sprite-loader
+          // 本地loader
+          // {
+          //   loader: path.resolve('../test-sprites-loader/index.js'),
+          //   options: {
+          //     dist: "./src/sprites" // 雪碧图保存路径（TODO:注意当output的clean设为true时，需要keep一下dist下面的路径，否则生成后可能会被删掉，应该是loader的执行时机问题）
+          //   }
+          // },
+        ],
       },
       // {
       //   test: /\.css$/i,
@@ -41,11 +48,11 @@ module.exports = {
       //     },
       //   ],
       // },
-    ]
+    ],
   },
   optimization: {
     splitChunks: {
-      chunks: 'all',
+      chunks: "all",
     },
   },
-}
+};
